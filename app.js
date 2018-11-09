0832a28a02bbfff9bb32111c1e40df5a7c5b9896
Federico Hernández Velásquez <federicohernandez.ve@gmail.com>
2018-11-09 10:05:54 -0300

    Removed cookieParser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -11,7 +10,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 
 app.use('/', indexRouter);
