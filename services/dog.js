2ece24f81cbf36548d80f7ba03912aa68d33e257
Federico Hernández Velásquez <federicohernandez.ve@gmail.com>
2018-11-09 11:30:38 -0300

    Added calls to dog api on dogService.

--- services/dog.js
+++ services/dog.js
@@ -1,15 +1,20 @@
-
+const axios   = require('axios');
 
 async function getDogInfo(breedname){
 	try {
+
+		const subBreeds = await axios.get(`https://dog.ceo/api/breed/${breedname}/list`);
+		const images    = await axios.get(`https://dog.ceo/api/breed/${breedname}/images`);
+
 		return { 
 			status: 200,
       breed: breedname,
-      subBreeds: [],
-      images: []
+      subBreeds: subBreeds.data.message,
+      images: images.data.message
 		};
 	}	catch(err){
-		throw new Error(err.message);
+		const { message = 'Request failed with status code 404' } = (err && err.response && err.response.data) ? err.response.data : {};
+		throw new Error(message);
 	}
 }
 
